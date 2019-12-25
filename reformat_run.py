23d254a20edc2fdf7e2b34894938733d09b8dd8b
Michael Lucy <mlucy@stripe.com>
2019-12-25 14:25:10 -0800

    Fixed war paint logic.

--- reformat_run.py
+++ reformat_run.py
@@ -38,10 +38,34 @@ def valid_neow_relic_del(data, rname):
     return ((data['neow_bonus'] in RELIC_REMOVING_NEOW_BONUSES) and
             (rname in STARTING_RELICS[data['character_chosen']]))
 
+def valid_relic_upgrade(data, states, cname):
+    floor = old_cname = None
+    if cname[-2:] == '+1':
+        old_cname = cname[:-2]
+        if is_type(cname, 'Skill'):
+            upgrades = states[-1]['scratch'].get('war_paint_upgrades', 0)
+            if upgrades > 0:
+                floor = states[-1]['scratch']['war_paint_floor']
+                states[-1]['scratch']['war_paint_upgrades'] = upgrades-1
+        if is_type(cname, 'Attack'):
+            upgrades = states[-1]['scratch'].get('whetstone_upgrades', 0)
+            if upgrades > 0:
+                states[-1]['scratch']['whetstone_upgrades'] = upgrades-1
+                floor = states[-1]['scratch']['whetstone_floor']
+    if states[floor]['deck'].get(old_cname, 0) == 0:
+        floor = old_cname = None
+    return floor, old_cname
+
 def note_card(state, cname, diff):
+    if 'Omamori' in state['relics'] and is_type(cname, 'Curse'):
+        uses = state['scratch'].get('omamori_uses', 0)
+        if uses < 2:
+            state['scratch']['omamori_uses'] = uses + 1
+            return False
     if cname not in state['deck']:
         state['deck'][cname] = 0
     state['deck'][cname] += diff
+    return True
 
 with open('data/cleaned/cards.json', 'r') as f:
     CARDS = json.load(f)
@@ -50,12 +74,12 @@ for x in CARDS:
         CARDS[x][y] += [z+'+1' for z in CARDS[x][y]]
 STARTING_DECKS = {
     'IRONCLAD': {'Strike': 5, 'Defend': 4, 'Bash': 1},
-    'SILENT': {'Strike': 5, 'Defend': 5, 'Survivor': 1, 'Neutralize': 1},
+    'THE_SILENT': {'Strike': 5, 'Defend': 5, 'Survivor': 1, 'Neutralize': 1},
     'DEFECT': {'Strike': 4, 'Defend': 4, 'Zap': 1, 'Dualcast': 1},
 }
 STARTING_RELICS = {
     'IRONCLAD': set(['Burning Blood']),
-    'SILENT': set(['Ring of the Snake']),
+    'THE_SILENT': set(['Ring of the Snake']),
     'DEFECT': set(['Cracked Core']),
 }
 ALL_CARDS = set()
@@ -66,7 +90,7 @@ for x in CARDS:
 
 def is_type(card, typ):
     for x in CARDS:
-        if card in CARDS[x][typ]:
+        if card in CARDS[x].get(typ, []):
             return True
 
 with open('data/code_relic_names.csv') as f:
@@ -74,6 +98,9 @@ with open('data/code_relic_names.csv') as f:
 
 with open('data/name_translation.json', 'r') as f:
     TRANSLATION = json.load(f)
+for k in list(TRANSLATION):
+    TRANSLATION[k+'+1'] = TRANSLATION[k]+'+1'
+
 def tr(name):
     name = ''.join([x[0].upper() + x[1:] for x in name.split()])
     return TRANSLATION.get(name, name)
@@ -140,6 +167,7 @@ def calc_state_by_floor(data):
     floor_state = [{
         'deck': STARTING_DECKS[data['character_chosen']],
         'relics': STARTING_RELICS[data['character_chosen']],
+        'scratch': {},
     }]
     for floor in range(1, data['floor_reached']):
         new_state = copy.deepcopy(floor_state[-1])
@@ -148,6 +176,12 @@ def calc_state_by_floor(data):
             if diff == 1:
                 assert(rname not in new_state['relics'])
                 new_state['relics'].add(rname)
+                if rname == 'War Paint':
+                    new_state['scratch']['war_paint_upgrades'] = 2
+                    new_state['scratch']['war_paint_floor'] = floor
+                elif rname == 'Whetstone':
+                    new_state['scratch']['whetstone_upgrades'] = 2
+                    new_state['scratch']['whetstone_floor'] = floor
             elif diff == -1:
                 # We might be removing a relic we got from the Neow bonus.
                 if rname not in new_state['relics']:
@@ -162,18 +196,26 @@ def calc_state_by_floor(data):
         for cname, diff in card_change_map[floor]:
             if (('Frozen Egg 2' in new_state['relics'] and is_type(cname, 'Power')) or
                 ('Toxic Egg 2' in new_state['relics'] and is_type(cname, 'Skill')) or
-                ('Molten Egg 2' in new_state['relics'] and is_type(cname, 'Attack'))): 
+                ('Molten Egg 2' in new_state['relics'] and is_type(cname, 'Attack'))):
                 if cname[-2:] != '+1':
                     cname += '+1'
-            note_card(new_state, cname, diff)
-            # We might have been given a card during our neow bonus.
-            if new_state['deck'][cname] < 0:
-                assert(valid_neow_add(data, cname))
-                for state in floor_state + [new_state]:
-                    note_card(state, cname, 1)
-            assert(new_state['deck'][cname] >= 0)
-            if new_state['deck'][cname] == 0:
-                del new_state['deck'][cname]
+            if note_card(new_state, cname, diff):
+                # We might have been given a card during our neow bonus.
+                if new_state['deck'][cname] < 0:
+                    # TODO: this might be incorrect if the neow bonus is also in play.
+                    upgrade_floor, old_cname = valid_relic_upgrade(
+                        data, floor_state + [new_state], cname)
+                    if upgrade_floor is not None:
+                        for state in floor_state[upgrade_floor:] + [new_state]:
+                            note_card(state, old_cname, -1)
+                            note_card(state, cname, 1)
+                    else:
+                        assert(valid_neow_add(data, cname))
+                        for state in floor_state + [new_state]:
+                            note_card(state, cname, 1)
+                assert(new_state['deck'][cname] >= 0)
+                if new_state['deck'][cname] == 0:
+                    del new_state['deck'][cname]
 
         floor_state.append(new_state)
 
@@ -189,12 +231,25 @@ def calc_state_by_floor(data):
     # pprint(master_deck)
     # pprint(calculated_deck)
     if master_deck != calculated_deck:
-        all_card_names = set(master_deck.keys()).union(set(calculated_deck.keys()))
+        all_card_names = list(set(master_deck.keys()).union(set(calculated_deck.keys())))
+        all_card_names.sort(reverse=True)
         for cname in all_card_names:
+            print(cname)
             master_count = master_deck.get(cname, 0)
             calculated_count = calculated_deck.get(cname, 0)
             if master_count != calculated_count:
+                print(cname, master_deck, calculated_deck, data['neow_bonus'], data['neow_cost'])
                 if master_count > calculated_count:
+                    if cname[-2:] == '+1':
+                        if ((master_deck.get(cname, 0) - calculated_deck.get(cname, 0)) ==
+                            (calculated_deck.get(cname[:-2], 0) - master_deck.get(cname[:-2], 0))):
+                            upgrade_floor, old_cname = valid_relic_upgrade(data, floor_state, cname)
+                            if upgrade_floor is not None:
+                                for state in floor_state[upgrade_floor:]:
+                                    diff = master_count - calculated_count
+                                    note_card(state, old_cname, -1*diff)
+                                    note_card(state, cname, diff)
+                                continue
                     assert(valid_neow_add(data, cname))
                 else:
                     assert(valid_neow_del(data, cname))
@@ -210,6 +265,10 @@ def calc_state_by_floor(data):
 
     # pprint(set(data['relics']))
     # pprint(floor_state[-1]['relics'])
+    # TODO: if we found out that we got Omamori from the Neow bonus
+    # then we need to go through and remove curses.  (In fact, I think
+    # this means we need to do the relic calculation before the
+    # per-floor card calculation.)
     master_relics = set(data['relics'])
     calculated_relics = floor_state[-1]['relics']
     if master_relics != calculated_relics:
